f3c105214fe48a8895ddbf19ee8e1aafdffb51d8
Asif Pirjade <130464059+Aasif7861224@users.noreply.github.com>
2025-06-22 10:07:52 +0530

    Add .next/static/chunks/src_app_checkout_page_jsx_8f875d17._.js 1750567065978

---
+++ .next/static/chunks/src_app_checkout_page_jsx_8f875d17._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_15251bc2._.js",
+  "static/chunks/src_app_checkout_page_jsx_791e095a._.js"
+],
+    source: "dynamic"
+});
